600f60ad3fdeb529bff52bed9b4f418105a72088
kadigrob <kadigrob@DESIGNER-MAIN>
2017-03-14 15:02:54 +0300

    new

--- MyFirstApplication/Source.cpp
+++ MyFirstApplication/Source.cpp
@@ -1,2 +1,17 @@
-// comment;
-// gfkfkf;
+#include <iostream>
+#include <string>  
+#include <string.h>
+
+using namespace std;
+
+int main()
+{	
+	// операторы вывода можно сцеплять
+	
+	cout << "Long 'Улисс' равна: \t"
+		 << strlen("Улисс") << '\n';
+
+	cout << "Size 'Улисс' равен: \t"
+		<< sizeof("Улисс") << endl;
+	//return 0;
+}
